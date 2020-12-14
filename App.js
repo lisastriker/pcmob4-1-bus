b1acbfa342c7e3fd53bcfebb7976383ff7005562
lisastriker <lisastriker@gmail.com>
2020-12-14 11:28:15 +0800

    activity and promise

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, ActivityIndicator, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true)
+  const [busTime, setBusTime] = useState("")
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=96169"
+  
+  function loadBusStopData(){
+    fetch(BUSSTOP_URL)
+    .then((response)=>{
+        return response.json();
+      })
+      .then((responseData)=>{
+        setLoading(false)
+        setBusTime(responseData.services[0].no)
+        console.log(responseData)
+      })
+  }
+  
+  useEffect(()=>{
+    loadBusStopData();
+  })
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.arrival}>Bus Arrival Time</Text>
+      <Text>{loading ? <ActivityIndicator size="large"/> : "Loaded"}</Text>
+      <Text>{busTime}</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text>Refresh</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +41,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    padding:10,
+    borderWidth:2,
+    borderColor:"black",
+    borderRadius:5,
+    backgroundColor:"orange",
+  },
+  arrival:{
+    fontSize:36,
+  }
 });
